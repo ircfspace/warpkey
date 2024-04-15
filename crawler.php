792d655cc2e82033d8e2a3a37e9e17c462e1d407
Immortal <ircfspace@gmail.com>
2024-04-15 12:07:07 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -1,14 +1,15 @@
 <?php
 
     $url = 'https://t.me/s/warpplus';
-    $pattern = '/Key: ([A-Za-z0-9\-]+) \(\d+\)/';
+    $pattern = '/🔐 Key: ([A-Za-z0-9\-]+) \(\d+ GB\)/';
     $getData = file_get_contents($url);
-    file_put_contents("list", $getData);die;
 
     $keys = [];
     if ( preg_match_all($pattern, $getData, $matches) ) {
         $keys = $matches[1];
     }
+
+    file_put_contents("list", $matches[1);die;
 
     $i = 1;
     $html = "";
