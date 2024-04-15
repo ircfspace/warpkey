53da99a08aeee19031540aca756c968f48a79179
Immortal <ircfspace@gmail.com>
2024-04-15 11:38:36 +0330

    init

---
+++ crawler.php
@@ -0,0 +1,21 @@
+<?php
+
+    $url = 'https://t.me/s/warpplus';
+    $pattern = '/🔐 Key: ([A-Za-z0-9\-]+) \(\d+ GB\)/';
+    $getData = file_get_contents($url);
+
+    $keys = [];
+    if ( preg_match_all($pattern, $getData, $matches) ) {
+        $keys = $matches[1];
+    }
+
+    $i = 1;
+    $html = "";
+    foreach($keys as $key) {
+        if ( $i > 15 ) {
+            break;
+        }
+        $html .= $key.( $key !== end($key) ? "\n" : "");
+
+    }
+    file_put_contents("warp+", $html);
